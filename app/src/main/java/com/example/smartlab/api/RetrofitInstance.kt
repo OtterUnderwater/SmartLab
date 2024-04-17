0c761668b2f976c3573f7d15e950507b56832f5a
Otter underwater <130929560+OtterUnderwater@users.noreply.github.com>
2024-04-17 21:45:06 +0300

    feat: connection to the api

---
+++ app/src/main/java/com/example/smartlab/api/RetrofitInstance.kt
@@ -0,0 +1,32 @@
+package com.example.smartlab.api
+
+import com.example.smartlab.objects.UrlData
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object RetrofitInstance {
+
+    // Создаем перехватчик HttpLoggingInterceptor (как сетевой LogCat)
+    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        // Существует несколько уровней перехвата данных: NONE, BASIC, HEADERS, BODY.
+        // Последний вариант самый информативный, но при больших потоках данных информация забьёт весь экран.
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    // Создаем клиента и подключаем перехватчик к веб-клиенту
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+
+    // Создаем объект для запроса к серверу
+    val api: Api = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(UrlData.BASE_URL)
+        .client(client)
+        .build()
+        .create(Api::class.java)
+    // Получаем объект Retrofit
+    // В методе create() указываем наш класс интерфейса с запросами к сайту
+}
