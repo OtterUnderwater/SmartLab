85faa4babdacfc0b9cbb3ff53895aafdcfb6b8e8
Otter underwater <130929560+OtterUnderwater@users.noreply.github.com>
2024-04-20 01:05:11 +0300

    feat: add category list

--- app/src/main/java/com/example/smartlab/screens/HomeAnalyses.kt
+++ app/src/main/java/com/example/smartlab/screens/HomeAnalyses.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -23,6 +25,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,15 +42,22 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.smartlab.models.Catalog
 import com.example.smartlab.ui.theme.GrayWhiteColor
+import com.example.smartlab.ui.theme.LightGrayColor
 import com.example.smartlab.ui.theme.SmartLabTheme
+import com.example.smartlab.ui.theme.TextButtonColor
 import com.example.smartlab.viewModel.ViewModelMain
 import kotlinx.coroutines.flow.collectLatest
+
 
 @Composable
 fun HomeAnalyses(navHostController: NavHostController?, viewModel: ViewModelMain?) {
     //получение catalog из api
     viewModel!!.getListCatalog()
-    val catalogList = viewModel!!.catalog.collectAsState().value
+    val catalogList = viewModel.catalog.collectAsState().value
+    //выбор всех категорий
+    val category = catalogList.distinctBy { it.category }.map { it.category }
+
+    var filteredCatalog = remember { mutableStateOf<List<Catalog>?>(null) }
 
     /** объект Context — предоставляет доступ к базовым функциям приложения в Android,
     таким как доступ к ресурсам, файловой системе, вызов активности и т. д.*/
@@ -68,13 +82,13 @@ fun HomeAnalyses(navHostController: NavHostController?, viewModel: ViewModelMain
         catalogList.forEach {
             Log.d("Анализы", it.name)
         }
-        PrintCatalog(catalogList)
+        PrintCatalog(catalogList, category)
     }
 }
 
 //Отрисовка каталога
 @Composable
-fun PrintCatalog(catalogList: List<Catalog>) {
+fun PrintCatalog(catalogList: List<Catalog>, category: List<String>) {
     Column(
         modifier = Modifier
             .fillMaxSize(1f)
@@ -98,6 +112,15 @@ fun PrintCatalog(catalogList: List<Catalog>) {
             }
         } else {
             //Иначе выводим данные в лист
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(top = 16.dp)
+            ) {
+                items(category.size) { index ->
+                    ItemCategory(category[index])
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
+            }
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -163,6 +186,26 @@ fun ItemCatalog(item: Catalog) {
                 )
             }
         }
+    }
+}
+
+//Отрисовка одной категории
+@Composable
+fun ItemCategory(item: String) {
+    Button(
+        onClick = {  },
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary, // Цвет ненажатой кнопки
+            contentColor = TextButtonColor, // Цвет текста на ненажатой кнопке
+        )
+    )
+    {
+        Text(
+            text = item,
+            fontSize = 15.sp,
+            color = Color.White
+        )
     }
 }
 
