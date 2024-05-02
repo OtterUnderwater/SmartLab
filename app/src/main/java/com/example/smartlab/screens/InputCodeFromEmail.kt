b2a1623ef3007eaf4f4a2114585588e434059365
Otter underwater <130929560+OtterUnderwater@users.noreply.github.com>
2024-05-02 21:02:13 +0300

    feat: add timer

--- app/src/main/java/com/example/smartlab/screens/InputCodeFromEmail.kt
+++ app/src/main/java/com/example/smartlab/screens/InputCodeFromEmail.kt
@@ -1,6 +1,6 @@
 package com.example.smartlab.screens
 
-import android.util.Log
+import android.os.CountDownTimer
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
@@ -39,7 +40,10 @@ import com.example.smartlab.viewModel.ViewModelMain
 fun InputCodeFromEmail(navHostController: NavHostController?, viewModel: ViewModelMain?) {
     //mutableStateOf() создает изменяемое состояние с нач значением
     var code: MutableList<String> = remember { mutableStateListOf("", "", "", "") }
-    val focusManager = LocalFocusManager.current
+    var time = remember { mutableStateOf(60) }
+    var focusManager = LocalFocusManager.current
+
+    //Перемещение фокуса между ячейками
     fun moveFocusToNextField(index: Int) {
         if (index < code.size - 1) {
             focusManager.moveFocus(FocusDirection.Next)
@@ -70,12 +74,13 @@ fun InputCodeFromEmail(navHostController: NavHostController?, viewModel: ViewMod
             OutlinedTextField(
                 value = code[0],
                 onValueChange = { newText ->
-                    if (newText.length == 1)
-                    {
+                    if (newText.length == 1) {
                         code[0] = newText
                         moveFocusToNextField(0)
+                    } else {
+                        code[0] = ""
                     }
-                                },
+                },
                 modifier = Modifier
                     .padding(horizontal = 8.dp)
                     .weight(25f)
@@ -86,6 +91,7 @@ fun InputCodeFromEmail(navHostController: NavHostController?, viewModel: ViewMod
                 maxLines = 1,
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
+
             OutlinedTextField(
                 value = code[1],
                 onValueChange = { newText ->
@@ -93,6 +99,9 @@ fun InputCodeFromEmail(navHostController: NavHostController?, viewModel: ViewMod
                     {
                         code[1] = newText
                         moveFocusToNextField(1)
+                    }
+                    else {
+                        code[1] = ""
                     }
                 },
                 modifier = Modifier
@@ -111,7 +120,10 @@ fun InputCodeFromEmail(navHostController: NavHostController?, viewModel: ViewMod
                     if (newText.length == 1)
                     {
                         code[2] = newText
-                        moveFocusToNextField(2)
+                        moveFocusToNextField(1)
+                    }
+                    else {
+                        code[2] = ""
                     }
                 },
                 modifier = Modifier
@@ -133,6 +145,9 @@ fun InputCodeFromEmail(navHostController: NavHostController?, viewModel: ViewMod
                         moveFocusToNextField(3)
                         viewModel!!.checkCodeAndEmail(code, navHostController!!) //запрос к апи
                     }
+                    else {
+                        code[3] = ""
+                    }
                 },
                 modifier = Modifier
                     .padding(horizontal = 8.dp)
@@ -150,12 +165,25 @@ fun InputCodeFromEmail(navHostController: NavHostController?, viewModel: ViewMod
                 .fillMaxWidth()
                 .padding(top = 16.dp),
             textAlign = TextAlign.Center,
-            text = "Отправить код повторно можно будет через 59 секунд",
+            text = "Отправить код повторно можно будет через ${time.value} секунд",
             fontSize = 15.sp, color = Color.Gray
         )
     }
-}
 
+    // Создание таймера обратного отсчета
+    // Подробнее - https://developer.android.com/reference/android/os/CountDownTimer
+    val timer = object : CountDownTimer(60000, 1000) {
+         override fun onTick(millisUntilFinished: Long) {
+             time.value = (millisUntilFinished / 1000).toInt()
+         }
+        override fun onFinish() {}
+    }
+
+    // Запуск таймера
+    LaunchedEffect(Unit) {
+        timer.start()
+    }
+}
 
 
 /*Для Preview*/
